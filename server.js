b88e81e7a369ca8c8b0801f28bf706092d43ec31
ABHINAV RAJ <abhinavrocky7488@gmail.com>
2022-12-16 03:37:48 +0530

    cors2

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-app.use(cors());
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
 const connectDB = require("./db");
 const app = express();
 app.use(express.json());
+app.use(cors());
 connectDB();
 
 app.use("/api/users", require("./routes/userRoutes"));
